8ec47426663b6eb307bc142de580801af4e1afb7
pko104 <peterrko1004@gmail.com>
2015-01-05 02:42:36 -0500

    first commit

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,28 @@
+class CarsController < ApplicationController
+
+  def create
+    @manufacturer = Manufacturer.find(params[:manufacturer_id])
+    @car = Car.new(car_params)
+    @car.manufacturer_id = @manufacturer.id
+
+    if @car.save
+      redirect_to manufacturer_path(id: @manufacturer.id), notice: "Your car was submitted."
+    else
+      flash.now[:notice]= @car.errors.full_messages
+      render :new
+    end
+  end
+
+  def new
+    @manufacturer = Manufacturer.find(params[:manufacturer_id])
+    @car = Car.new
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:mileage, :manufacturer_id, :color, :year, :description)
+  end
+
+end
+
---
+++ app/controllers/manufacturers_controller.rb
@@ -0,0 +1,35 @@
+class ManufacturersController < ApplicationController
+
+  def create
+    @manufacturer = Manufacturer.new(manufacturer_params)
+
+    if @manufacturer.save
+      redirect_to '/', notice: "Your manufacturer was submitted."
+    else
+      flash.now[:notice]= @manufacturer.errors.full_messages
+      render :new
+    end
+  end
+
+  def new
+    @manufacturer = Manufacturer.new
+  end
+
+  def index
+    @manufacturers = Manufacturer.all
+  end
+
+  def show
+    @manufacturer = Manufacturer.find(params[:id])
+    @cars = Car.where(params[:manufacturer_id])
+  end
+
+  private
+
+  def manufacturer_params
+    params.require(:manufacturer).permit(:name, :country_name)
+  end
+
+end
+
+
